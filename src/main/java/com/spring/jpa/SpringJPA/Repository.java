6440d499c845eb9cecd3aff4bf1a0a0a1caeeb1a
vinod <vinod.gangadhariah@wibmo.com>
2018-04-18 14:38:00 +0530

    Second and hopefully final commit!

---
+++ src/main/java/com/spring/jpa/SpringJPA/Repository.java
@@ -0,0 +1,7 @@
+package com.spring.jpa.SpringJPA;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repository extends CrudRepository<Person,String> {
+
+}
---
+++ src/main/java/com/spring/jpa/SpringJPA/ServiceProvider.java
@@ -0,0 +1,28 @@
+package com.spring.jpa.SpringJPA;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceProvider {
+
+@Autowired
+Repository repository;
+
+public Person addPerson(Person person) {
+	
+	return repository.save(person);
+}
+
+public Optional<Person> getPerson(String id) {
+	return repository.findById(id);
+}
+
+public String deletePerson(String id) {
+
+	 repository.deleteById(id);
+	return "Deleted";
+}
+}
